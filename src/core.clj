2db3195eaca76964be82b279883be5e793d7ce06
anders <andersmurphy@gmail.com>
2018-11-17 12:08:26 +0000

    Fix mistake in language urls

--- src/core.clj
+++ src/core.clj
@@ -85,8 +85,8 @@
    [:link {:rel  "stylesheet"
            :href (str highlight-url "/styles/atom-one-light.min.css")}]
    [:script {:src (str highlight-url "/highlight.min.js")}]
-   [:script {:src (str highlight-url "/clojure.min.js")}]
-   [:script {:src (str highlight-url "/clojure-repl.min.js")}]
+   [:script {:src (str highlight-url "/languages/clojure.min.js")}]
+   [:script {:src (str highlight-url "/languages/clojure-repl.min.js")}]
    [:script "hljs.initHighlightingOnLoad();"]
    ;; icons
    [:link {:rel   "apple-touch-icon-precomposed"
