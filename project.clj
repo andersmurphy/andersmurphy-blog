8553adf337eb5d0b745e9ff524c8a0f5dacff0b4
andersmurphy <andersmurphy@gmail.com>
2021-12-05 18:59:28 +0000

    Update clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject andersmurphy-blog "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0-beta5"]
+  :dependencies [[org.clojure/clojure "1.11.0-alpha3"]
                  [org.clojure/data.xml "0.0.8"]
                  [hiccup "1.0.5"]
                  [markdown-clj "1.10.7"]]
