b36715b2c90c8f9ff2f7de99ff39c6afc6d297ee
andersmurphy <andersmurphy@gmail.com>
2025-02-11 14:13:20 +0000

    Fix dark/light toggle

--- docs/toggle.js
+++ docs/toggle.js
@@ -20,7 +20,7 @@ function modeSwitch() {
   iconChange(targetTheme)
 }
 
-document.addEventListener("turbo:load", (event) => {
+document.addEventListener("load", (event) => {
   const toggle = document.querySelector("#toggle")
   toggle.addEventListener("click", modeSwitch)
   const currentTheme = document.documentElement.getAttribute("data-theme")
